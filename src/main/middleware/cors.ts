5112c8ec90ddd1224f8a4614a7477b670a190609
JoseMarshall <hebojosemar@gmail.com>
2021-11-20 20:01:09 +0100

    refactor: cors config

--- src/main/middleware/cors.ts
+++ src/main/middleware/cors.ts
@@ -1,17 +1,22 @@
 import cors from 'cors';
 
-import { logger } from '../../utils/logger';
+// import { logger } from '../../utils/logger';
 
-const whitelist = process.env.ALLOWED_URLS?.split(';'); // the array containing all url allowed by cors
+// const whitelist = process.env.ALLOWED_URLS?.split(';'); // the array containing all url allowed by cors
 
 const corsOptions = {
   origin(origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) {
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      logger.error(`${origin} Not allowed by CORS`);
-      callback(new Error(`${origin} Not allowed by CORS`));
-    }
+    // For now lets allow any URL
+    // Comment the following line to restrict access by CORS
+    callback(null, true);
+
+    // //Uncomment the following lines if you want to restrict access by CORS
+    // if (whitelist.indexOf(origin) !== -1 || !origin) {
+    //   callback(null, true);
+    // } else {
+    //   logger.error(`${origin} Not allowed by CORS`);
+    //   callback(new Error(`${origin} Not allowed by CORS`));
+    // }
   },
   methods: ['PUT', 'GET', 'POST', 'HEAD', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
